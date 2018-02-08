3e471da79d3f6950af86f87e8a76d2f90bd978c4
@soyjavi <javi.jimenez.villar@gmail.com>
2018-02-08 09:56:06 +0700

    New component <Input>

---
+++ src/components/Input/style.js
@@ -0,0 +1,62 @@
+import { Platform, StyleSheet } from 'react-native';
+
+import { THEME } from '../../common';
+
+const {
+  COLOR, FONT, LAYOUT, OFFSET, UNIT,
+} = THEME;
+
+export default StyleSheet.create({
+  container: {
+    minHeight: LAYOUT.INPUT_HEIGHT,
+    marginBottom: OFFSET,
+  },
+
+  label: {
+    color: COLOR.TEXT_LIGHTEN,
+    fontSize: FONT.SIZE.SMALL,
+    marginVertical: UNIT / 4,
+  },
+
+  labelError: {
+    color: COLOR.ERROR,
+  },
+
+  labelFocus: {},
+
+  input: {
+    borderColor: COLOR.BORDER,
+    borderWidth: 1,
+    borderRadius: UNIT / 4,
+    color: COLOR.TEXT,
+    fontSize: FONT.SIZE.REGULAR,
+    fontWeight: FONT.WEIGHT.REGULAR,
+    paddingVertical: UNIT * 0.75,
+    paddingHorizontal: UNIT * 0.5,
+    width: '100%',
+
+    ...Platform.select({
+      web: {
+        fontFamily: 'inherit',
+        outline: 'none',
+      },
+    }),
+  },
+
+  inputError: {
+    borderColor: COLOR.ERROR,
+  },
+
+  inputFocus: {
+    borderColor: COLOR.PRIMARY,
+  },
+
+  inputDisabled: {
+    color: COLOR.BASE,
+    backgroundColor: COLOR.BACKGROUND,
+  },
+
+  active: {
+    color: COLOR.ACCENT,
+  },
+});
