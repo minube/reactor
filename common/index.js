c9591af2a83de605e4a7c20d26437a574606af27
@soyjavi <javi.jimenez.villar@gmail.com>
2018-06-11 14:47:31 +0700

    We don’t need fetch any more… cause we have <BoardingPass>

--- common/index.js
+++ common/index.js
@@ -1,11 +1,9 @@
-import fetch from './fetch';
 import LAYOUT from './layout';
 import SHAPE from './shape';
 import STYLE from './style';
 import THEME from './theme';
 
 export {
-  fetch,
   LAYOUT,
   SHAPE,
   STYLE,
