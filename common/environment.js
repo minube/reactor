d370361c437ade78f020ea4c80ac5c546e070b70
@soyjavi <javi.jimenez.villar@gmail.com>
2018-07-04 12:19:28 +0700

    New ENV (beta)

---
+++ common/environment.js
@@ -0,0 +1,14 @@
+import { Platform } from 'react-native';
+
+const IS_SERVER = window === 'undefined';
+const IS_WEB = Platform.OS === 'web';
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+export default {
+  IS_NATIVE: !IS_WEB,
+  IS_SERVER: IS_SERVER && IS_WEB,
+  IS_WEB,
+
+  IS_PRODUCTION,
+  IS_DEVELOPMENT: !IS_PRODUCTION,
+};
--- common/index.js
+++ common/index.js
@@ -1,9 +1,11 @@
+import ENV from './environment';
 import LAYOUT from './layout';
 import SHAPE from './shape';
 import STYLE from './style';
 import THEME from './theme';
 
 export {
+  ENV,
   LAYOUT,
   SHAPE,
   STYLE,
