1abd4005bd4a63166f878f70c1af59c3f3ecd57a
@soyjavi <javi.jimenez.villar@gmail.com>
2018-07-04 12:19:49 +0700

    Export all resources from root

---
+++ index.js
@@ -0,0 +1,71 @@
+// -- Common
+import ENV from './common/environment';
+import LAYOUT from './common/layout';
+import SHAPE from './common/shape';
+import STYLE from './common/style';
+import THEME from './common/theme';
+// -- Components
+import Activity from './components/Activity';
+import Avatar from './components/Avatar';
+import BoardingPass, { fetch } from './components/BoardingPass';
+import Breadcrumbs from './components/Breadcrumbs';
+import Button from './components/Button';
+import Dialog from './components/Dialog';
+import Heading from './components/Heading';
+import Icon from './components/Icon';
+import Image from './components/Image';
+import Input from './components/Input';
+import Link from './components/Link';
+import ListingCard from './components/ListingCard';
+import Markdown from './components/Markdown';
+import Motion from './components/Motion';
+import PictureCard from './components/PictureCard';
+import Price from './components/Price';
+import ProgressBar from './components/ProgressBar';
+import Rating from './components/Rating';
+import LayoutView from './components/Layout';
+import Share from './components/Share';
+import Slider from './components/Slider';
+import Switch from './components/Switch';
+import Text from './components/Text';
+import Touchable from './components/Touchable';
+import Video from './components/Video';
+import Viewport from './components/Viewport';
+
+export {
+  // Components
+  Activity,
+  Avatar,
+  BoardingPass,
+  Breadcrumbs,
+  Button,
+  Dialog,
+  Heading,
+  Icon,
+  Image,
+  Input,
+  Link,
+  ListingCard,
+  Markdown,
+  Motion,
+  PictureCard,
+  Price,
+  ProgressBar,
+  LayoutView,
+  Rating,
+  Share,
+  Slider,
+  Switch,
+  Text,
+  Touchable,
+  Video,
+  Viewport,
+  // Methods
+  fetch,
+  // Constants
+  ENV,
+  LAYOUT,
+  SHAPE,
+  STYLE,
+  THEME,
+};
