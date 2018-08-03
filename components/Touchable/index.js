3659e0006fcf73cc8b7b3d19502caedd90023c8a
@soyjavi <javi.jimenez.villar@gmail.com>
2018-08-03 09:18:04 +0700

    <Touchable> is not available in ServerSide

--- components/Touchable/index.js
+++ components/Touchable/index.js
@@ -1,1 +1,5 @@
-export { default } from './Touchable';
+import { createElement } from 'react';
+import { ENV } from '../../common';
+import Touchable from './Touchable';
+
+export default props => ENV.IS_SERVER ? props.children : createElement(Touchable, props); // eslint-disable-line
