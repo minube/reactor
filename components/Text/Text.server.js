1174eb6ea1696c503d787a1b02b548f975a72b7d
@soyjavi <javi.jimenez.villar@gmail.com>
2018-08-20 10:01:06 +0700

    Hydrate style if it’s an object

--- components/Text/Text.server.js
+++ components/Text/Text.server.js
@@ -8,6 +8,7 @@ const STYLE = { color: COLOR.TEXT, marginTop: 0, marginBottom: 0 };
 export default ({
   color, title, subtitle, large, small, tiny, ...inherit
 }) => {
+  const style = (typeof inherit.style === 'object') ? inherit.style : {};
   let tag = 'p';
 
   if (title) tag = 'h1';
@@ -15,5 +16,5 @@ export default ({
   else if (large) tag = 'h3';
   else if (small || tiny) tag = 'small';
 
-  return createElement(tag, { style: { ...STYLE, color } }, inherit.children);
+  return createElement(tag, { style: { ...STYLE, ...style, color } }, inherit.children);
 };
