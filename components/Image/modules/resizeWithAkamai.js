268b7d618904bb5277bd4db93b5766d9e11cc7cf
@soyjavi <javi.jimenez.villar@gmail.com>
2018-08-23 12:23:36 +0700

    Clever way for get 3 kinds of cache with Akamai
    Using screen size

--- components/Image/modules/resizeWithAkamai.js
+++ components/Image/modules/resizeWithAkamai.js
@@ -1,9 +1,19 @@
 import { LAYOUT } from '../../../common';
 
 const AKAMAI_DOMAIN = 'imgs-akamai.mnstatic.com';
+const LOW = { W: 375, H: 736 };
+const REGULAR = { W: 1024, H: 768 };
+const HIGH = { W: 1920, H: 1200 };
 
 export default ({ uri = '', ratio = 1 }) => {
   if (!uri.includes(AKAMAI_DOMAIN)) return uri;
 
-  return `${uri}?fit=around|${LAYOUT.VIEWPORT.W / ratio}:${LAYOUT.VIEWPORT.H / ratio}`;
+  const { VIEWPORT: { TINY, PHONE, LARGE } } = LAYOUT;
+  let { VIEWPORT: { W, H } } = LAYOUT;
+
+  if (TINY || PHONE) ({ W, H } = LOW);
+  else if (LARGE) ({ W, H } = HIGH);
+  else ({ W, H } = REGULAR);
+
+  return `${uri}?fit=around|${parseInt(W / ratio, 10)}:${parseInt(H / ratio, 10)}`;
 };
