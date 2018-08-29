f0b6b3d13700e834b8e84d5bce00db19f5fabd2f
@soyjavi <javi.jimenez.villar@gmail.com>
2018-08-29 13:01:09 +0700

    New environment IS_MOBILE_WEB

--- common/environment.js
+++ common/environment.js
@@ -5,15 +5,15 @@ const { NODE_ENV, REACT_APP_ENV = 'development' } = process.env;
 const IS_SERVER = typeof window === 'undefined';
 const IS_WEB = Platform.OS === 'web';
 const IS_PRODUCTION = NODE_ENV === 'production' && NODE_ENV === REACT_APP_ENV;
-const IS_MOBILE = IS_WEB
+const IS_MOBILE_WEB = IS_WEB && !IS_SERVER
   ? /iPhone|Android|Tablet|iPad|Mobile|iPod|Windows Phone|Blackberry/i.test(DeviceInfo.userAgent)
-  : true;
+  : false;
 
 export default {
   IS_NATIVE: !IS_WEB,
   IS_SERVER: IS_SERVER && IS_WEB,
   IS_WEB,
-  IS_MOBILE,
+  IS_MOBILE_WEB,
 
   IS_PRODUCTION,
   IS_DEVELOPMENT: !IS_PRODUCTION,
