058d30ed8b3fb002fb58605b4ea0d56217950d35
@soyjavi <javi.jimenez.villar@gmail.com>
2018-09-03 12:25:06 +0700

    Use LayoutConsumer

--- .storybook/decorator-layout.js
+++ .storybook/decorator-layout.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 
-import { LAYOUT, STYLE, THEME } from '../common';
-import { LayoutView, Text } from '../components';
+import { STYLE, THEME } from '../common';
+import { LayoutConsumer, LayoutView, Text } from '../components';
 
 const { COLOR: { WHITE }, UNIT, OFFSET } = THEME;
 
@@ -30,22 +30,24 @@ const styles = StyleSheet.create({
 })
 
 export default (storyFn) => (
-  <LayoutView
-    style={styles.container}
-  >
-    <View style={styles.info}>
-      <View style={STYLE.ROW}>
-        <Text color={WHITE} bold small>{LAYOUT.VIEWPORT.PORTRAIT ? 'PORTRAIT ' : 'LANDSCAPE '}</Text>
-        { LAYOUT.VIEWPORT.IPHONEX && <Text color={WHITE} tiny>IPHONEX</Text> }
-        { LAYOUT.VIEWPORT.TINY && <Text color={WHITE} tiny>TINY</Text> }
-        { LAYOUT.VIEWPORT.PHONE && <Text color={WHITE} tiny>PHONE</Text> }
-        { LAYOUT.VIEWPORT.TABLET && <Text color={WHITE} tiny>TABLET</Text> }
-        { LAYOUT.VIEWPORT.SMALL && <Text color={WHITE} tiny>SMALL</Text> }
-        { LAYOUT.VIEWPORT.REGULAR && <Text color={WHITE} tiny>REGULAR</Text> }
-        { LAYOUT.VIEWPORT.LARGE && <Text color={WHITE} tiny>LARGE</Text> }
+  <LayoutView style={styles.container}>
+    <LayoutConsumer>
+    { ({ viewport }) =>
+      <View style={styles.info}>
+        <View style={STYLE.ROW}>
+          <Text color={WHITE} bold small>{viewport.PORTRAIT ? 'PORTRAIT ' : 'LANDSCAPE '}</Text>
+          { viewport.IPHONEX && <Text color={WHITE} tiny>IPHONEX</Text> }
+          { viewport.TINY && <Text color={WHITE} tiny>TINY</Text> }
+          { viewport.PHONE && <Text color={WHITE} tiny>PHONE</Text> }
+          { viewport.TABLET && <Text color={WHITE} tiny>TABLET</Text> }
+          { viewport.SMALL && <Text color={WHITE} tiny>SMALL</Text> }
+          { viewport.REGULAR && <Text color={WHITE} tiny>REGULAR</Text> }
+          { viewport.LARGE && <Text color={WHITE} tiny>LARGE</Text> }
+        </View>
+        <Text tiny color={WHITE}>{`${viewport.W} x ${viewport.H}`}</Text>
       </View>
-      <Text tiny color={WHITE}>{`${LAYOUT.VIEWPORT.W} x ${LAYOUT.VIEWPORT.H}`}</Text>
-    </View>
+    }
+    </LayoutConsumer>
     { storyFn() }
   </LayoutView>
 );
