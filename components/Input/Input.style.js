b6abc1d2eba196088c1334389323cc778e4945bf
@soyjavi <javi.jimenez.villar@gmail.com>
2018-12-10 07:37:40 +0700

    Remove web behavior on focus

--- components/Input/Input.style.js
+++ components/Input/Input.style.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 import { THEME } from '../../common';
 
@@ -19,6 +19,11 @@ export default StyleSheet.create({
     paddingVertical: UNIT * 0.75,
     paddingHorizontal: UNIT * 0.5,
     width: '100%',
+    ...Platform.select({
+      web: {
+        outline: 'none',
+      },
+    }),
     ...FONT.INPUT,
   },
 
