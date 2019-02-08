1c8120ae9e728ae61b3e6cd3e8c6c3df33320b0c
@soyjavi <javi.jimenez.villar@gmail.com>
2019-02-08 09:09:26 +0700

    Improved <Switch> composition

--- components/Switch/Switch.style.js
+++ components/Switch/Switch.style.js
@@ -7,13 +7,16 @@ const { UNIT } = THEME;
 export default StyleSheet.create({
   container: {
     ...LAYOUT.STYLE.ROW,
-    height: UNIT * 3.9,
-    marginBottom: UNIT,
     alignSelf: 'flex-end',
+    height: UNIT * 4.4,
+    marginBottom: UNIT * 2.4,
+  },
+
+  label: {
+    marginBottom: 0,
   },
 
   switch: {
-    height: UNIT * 2.4,
-    marginRight: UNIT,
+    height: UNIT * 2.8,
   },
 });
