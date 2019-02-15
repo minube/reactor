169b6e5953b681aab26827dbf14f3711a0bde88d
@soyjavi <javi.jimenez.villar@gmail.com>
2019-02-15 12:12:18 +0700

    Use /api/tracking instead of a straight request to amplitude 3rd party

--- context/Amplitude/modules/fetch.js
+++ context/Amplitude/modules/fetch.js
@@ -1,6 +1,6 @@
 import ENV from '../../../common/environment';
-import queryString from '../../../common/queryString';
 import PKG from '../../../package.json';
+import { fetch } from '../../../components/BoardingPass';
 
 import entropy from './entropy';
 
@@ -10,54 +10,38 @@ const {
   deviceManufacturer, deviceModel,
   osName, osVersion,
   locale, platform, totalMemory, userAgent,
-  ...userProperties
+  ...screenProperties
 } = entropy;
-const ENDPOINT_DEFAULT = 'httpapi';
-const IDENTIFY = 'identify';
-const ENDPOINT_PROPERTY = {
-  httpapi: 'event',
-  identify: 'identification',
-};
 
-export default async (key, event = {}, endpoint = ENDPOINT_DEFAULT) => {
+export default async (event = {}, method = 'event') => {
   const props = {
     device_manufacturer: deviceManufacturer,
     device_model: deviceModel,
     language: locale,
     library: { name: PKG.name, version: PKG.version },
-    ip: '$remote',
     os_name: IS_WEB ? browserName : osName,
     os_version: IS_WEB ? browserVersion : osVersion,
     platform: IS_WEB ? 'Web' : osName,
-    timestamp: new Date().getTime(),
-    user_agent: userAgent,
-    // version_name: null // Used for determine the app, we'll use event_properties.pkg
     ...event,
-    user_properties: endpoint === IDENTIFY
-      ? {
-        $set: {
-          browser: { name: browserName, version: browserVersion },
-          os: { name: osName, version: osVersion },
-          screen: userProperties,
-          totalMemory,
-          ...event.userProperties,
-        },
-      }
-      : {},
   };
+
+  if (method === 'session') {
+    props.user_properties = {
+      $set: {
+        browser: { name: browserName, version: browserVersion },
+        os: { name: osName, version: osVersion },
+        screen: screenProperties,
+        totalMemory,
+        ...event.userProperties,
+      },
+    };
+  }
 
   if (!IS_PRODUCTION && !NODE_ENV) console.info(`⚡️ProviderAmplitude:${endpoint}`, props); // eslint-disable-line
 
-  fetch(`https://api.amplitude.com/${endpoint}`, {
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+  fetch({
     method: 'POST',
-    body: queryString({
-      api_key: key,
-      // client: '68fcc232a01a8a3490a4b59c9b819137',
-      [ENDPOINT_PROPERTY[endpoint]]: JSON.stringify(props),
-      // v: 2,
-      // upload_time: new Date().getTime(),
-      // checksum: '46d63a17125ce9c96e70657869f7d203',
-    }),
+    service: `tracking/${method}`,
+    body: JSON.stringify(props),
   });
 };
