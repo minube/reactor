d09179dd74c3e5188b1576bf09962daa3a9eb6e1
@soyjavi <javi.jimenez.villar@gmail.com>
2019-03-07 10:16:06 +0700

    Install dependencies

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,34 +1,30 @@
-// const path = require('path');
-// const webpack = require('webpack'); // eslint-disable-line
+const path = require('path');
+const webpack = require('webpack'); // eslint-disable-line
 
-// module.exports = (config, type) => {
-//   const { env: { NODE_ENV = 'development ' } } = process;
+module.exports = ({ config, mode }) => {
+  const { env: { NODE_ENV = 'development ' } } = process;
 
-//   config.resolve = {
-//     modules: ['node_modules'],
-//     extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
-//     alias: {
-//       'react-native': 'react-native-web',
-//     },
-//   };
+  config.resolve = {
+    modules: ['node_modules'],
+    extensions: ['.web.js', '.js', '.json', '.web.jsx', '.jsx'],
+    alias: {
+      'react-native': 'react-native-web',
+    },
+  };
 
-//   if (config.plugins && config.module.rules) {
-//     config.module.rules.push({
-//       test: /\.(gif|jpe?g|png|svg)$/,
-//       use: {
-//         loader: 'url-loader',
-//         options: { name: '[name].[ext]' },
-//       },
-//     });
+  if (config.plugins && config.module.rules) {
+    config.module.rules.push({
+      test: /\.(gif|jpe?g|png|svg)$/,
+      use: {
+        loader: 'url-loader',
+        options: { name: '[name].[ext]' },
+      },
+    });
 
-//     config
-//       .plugins
-//       .push(new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }));
-//   }
+    config
+      .plugins
+      .push(new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }));
+  }
 
-//   return config;
-// };
-
-
-// module.exports = ({ config, mode }) => { config.module.rules.push(...); return config; }
-module.exports = ({ config, mode }) => { return config; }
+  return config;
+};
