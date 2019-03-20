40cfd408e4c2e354e036af422783587782177a0d
veroglez <veronica.glezperez@gmail.com>
2019-03-20 16:13:29 +0100

    Modifying color label

--- components/Input/InputLabel.style.js
+++ components/Input/InputLabel.style.js
@@ -2,10 +2,11 @@ import { StyleSheet } from 'react-native';
 
 import { THEME } from '../../common';
 
-const { UNIT } = THEME;
+const { COLOR, UNIT } = THEME;
 
 export default StyleSheet.create({
   container: {
     marginBottom: UNIT * 0.6,
+    color: COLOR.TEXT,
   },
 });
