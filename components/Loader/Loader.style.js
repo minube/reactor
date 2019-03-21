c9dacd39a3815ef2712d7a879ecd3a261a084637
veroglez <veronica.glezperez@gmail.com>
2019-03-21 16:03:06 +0100

    styles

--- components/Loader/Loader.style.js
+++ components/Loader/Loader.style.js
@@ -1,7 +1,21 @@
 import { StyleSheet } from 'react-native';
 
+import { THEME } from '../../common';
+
+const { UNIT, FONT } = THEME;
+
 export default StyleSheet.create({
   container: {
+    ...FONT.BUTTON,
     flexDirection: 'row',
+  },
+
+  regular: {
+    ...FONT.BUTTON,
+    fontSize: UNIT * 1.6,
+  },
+
+  large: {
+    fontSize: UNIT * 6,
   },
 });
