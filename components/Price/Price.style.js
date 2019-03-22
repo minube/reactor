ec9cbcba2d31d86cac1f8d8bc142196b70c7e42f
veroglez <veronica.glezperez@gmail.com>
2019-03-22 15:07:40 +0100

    Style symbol

--- components/Price/Price.style.js
+++ components/Price/Price.style.js
@@ -1,11 +1,17 @@
 import { StyleSheet } from 'react-native';
 
-import { LAYOUT } from '../../common';
+import { LAYOUT, THEME } from '../../common';
+
+const { UNIT } = THEME;
 
 export default StyleSheet.create({
   container: LAYOUT.STYLE.ROW,
 
   minimize: {
     transform: [{ scale: 0.8 }],
+  },
+
+  symbol: {
+    marginLeft: UNIT * 0.4,
   },
 });
