a47a8504a5783ce0166dc0295a0c97d7ac3170bb
@soyjavi <javi.jimenez.villar@gmail.com>
2019-04-02 12:25:04 +0700

    Fixed <Price> using mocks

---
+++ components/Price/Price.mocks.js
@@ -0,0 +1,20 @@
+import { THEME } from '../../common';
+
+export default {
+  FIXED: 4,
+  FIXED_NULL: 0,
+  LOCALE: 'es-ES',
+  STYLE: {
+    fontWeight: THEME.FONT.WEIGHT.BOLD,
+    fontSize: THEME.UNIT * 2,
+    color: THEME.COLOR.PRIMARY,
+  },
+  SYMBOL_$: '$',
+  SYMBOL_EUR: 'eur',
+  TEXT_PROPS: {
+    headline: true,
+    level: 3,
+  },
+  TITLE: 'From',
+  VALUE: 6995.12345,
+};
