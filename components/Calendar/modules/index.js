db4c4fec509360e7b9c0752b8114a7b2c66104fa
@soyjavi <javi.jimenez.villar@gmail.com>
2019-04-15 10:45:00 +0700

    New methods

--- components/Calendar/modules/index.js
+++ components/Calendar/modules/index.js
@@ -1,9 +1,13 @@
 import decomposeDate from './decomposeDate';
 import firstDateOfWeek from './firstDateOfWeek';
 import LOCALE from './locale';
+import nextMonth from './nextMonth';
+import previousMonth from './previousMonth';
 
 export {
   decomposeDate,
   firstDateOfWeek,
   LOCALE,
+  nextMonth,
+  previousMonth,
 };
