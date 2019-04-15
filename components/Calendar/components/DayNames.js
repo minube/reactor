4a766991ef23081a73cef4dd4d537366b1e902fd
@soyjavi <javi.jimenez.villar@gmail.com>
2019-04-15 08:48:51 +0700

    Use proper name `cell`

--- components/Calendar/components/DayNames.js
+++ components/Calendar/components/DayNames.js
@@ -8,7 +8,13 @@ import styles from './DayNames.style';
 const DayNames = ({ locale = [], ...inherit }) => (
   <View style={styles.container}>
     { locale.map(day => (
-      <Text key={day} subtitle level={2} {...inherit.styleDayNames} style={[styles.box, inherit.range && styles.boxExpand]}>
+      <Text
+        key={day}
+        subtitle
+        level={2}
+        {...inherit.styleDayNames}
+        style={[styles.cell, inherit.range && styles.cellExpand]}
+      >
         {day}
       </Text>
     ))}
