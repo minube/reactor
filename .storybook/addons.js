40373b90ff47b9f288a46d9593a95c255c0fedbc
@soyjavi <javi.jimenez.villar@gmail.com>
2019-06-03 12:22:14 +0700

    Start updating react-native-web

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,6 +2,5 @@ import '@storybook/addon-options/register';
 import '@storybook/addon-knobs/register';
 import '@storybook/addon-viewport/register';
 import '@storybook/addon-actions/register';
-import '@storybook/addon-notes/register';
 import '@storybook/addon-console';
 import '@dump247/storybook-state/register';
