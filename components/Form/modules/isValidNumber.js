455cda6a2727bb5983378d55dd622af1c2ef17ce
david <dgalindoc@gmail.com>
2020-01-30 09:00:27 +0100

    test

--- components/Form/modules/isValidNumber.js
+++ components/Form/modules/isValidNumber.js
@@ -1,5 +1,5 @@
 
-export default (number = '', { minChar = 0 }, { maxChar = 999 }) => {
-  const REGEXP = new RegExp(`^[0-9]{${minChar}, ${maxChar}}`);
+export default (number = '', { minChar = 0 }, maxChar = 999) => {
+  const REGEXP = new RegExp(`^[0-9]{${minChar},${maxChar}}$`);
   return number !== '' ? REGEXP.test(number) : true;
 };
--- components/Form/modules/isValidNumber.test.js
+++ components/Form/modules/isValidNumber.test.js
@@ -2,35 +2,35 @@ import isValidNumber from './isValidNumber';
 
 describe('Components:Form:modules:isValidNumber', () => {
   it('default', () => {
-    const value = isValidNumber('', {});
+    const value = isValidNumber('', {}, {});
     expect(value).toEqual(true);
   });
 
   it('using an incorrect value', () => {
-    let value = isValidNumber('111', { minChar: 4 });
+    let value = isValidNumber('111', { minChar: 4 }, 4);
     expect(value).toEqual(false);
 
-    value = isValidNumber('111222', { minChar: 10 });
+    value = isValidNumber('111222', { minChar: 10 }, 19);
     expect(value).toEqual(false);
 
-    value = isValidNumber('a', { minChar: 2 });
+    value = isValidNumber('a', { minChar: 2 }, 4);
     expect(value).toEqual(false);
   });
 
   it('using a correct value', () => {
-    let value = isValidNumber('111222333', { minChar: 8 });
+    let value = isValidNumber('111222333', { minChar: 2 });
     expect(value).toEqual(true);
 
-    value = isValidNumber('11122233', { minChar: 6 });
+    value = isValidNumber('1122233', { minChar: 6 }, 7);
     expect(value).toEqual(true);
 
-    value = isValidNumber('111222333', { minChar: 2 });
+    value = isValidNumber('111222333', { minChar: 2 }, 9);
     expect(value).toEqual(true);
 
-    value = isValidNumber('111222333', { minChar: 3 });
+    value = isValidNumber('111222333', { minChar: 3 }, 10);
     expect(value).toEqual(true);
 
-    value = isValidNumber('0111222333', { minChar: 10 });
+    value = isValidNumber('0111222333', { minChar: 10 }, 10);
     expect(value).toEqual(true);
   });
 });
