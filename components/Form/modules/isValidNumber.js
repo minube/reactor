1fd806abbbea54fd87092d6f614b8e448aee2f7c
david <dgalindoc@gmail.com>
2020-01-30 14:36:23 +0100

    test

--- components/Form/modules/isValidNumber.js
+++ components/Form/modules/isValidNumber.js
@@ -1,4 +1,3 @@
-
 export default (number = '', { minChar = 0, maxChar = 999 }) => {
   const REGEXP = new RegExp(`^[0-9]{${minChar},${maxChar}}$`);
   return number !== '' ? REGEXP.test(number) : true;
