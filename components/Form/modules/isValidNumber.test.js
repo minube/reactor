a9fd9f3797f1ed0ef859ad5a1ccd0b53685f4c86
david <dgalindoc@gmail.com>
2020-01-30 14:03:12 +0100

    MIN-13804 - Refactor config

--- components/Form/modules/isValidNumber.test.js
+++ components/Form/modules/isValidNumber.test.js
@@ -7,13 +7,13 @@ describe('Components:Form:modules:isValidNumber', () => {
   });
 
   it('using an incorrect value', () => {
-    let value = isValidNumber('111', { minChar: 4 });
+    let value = isValidNumber('111', { minChar: 4, maxChar: 10 });
     expect(value).toEqual(false);
 
-    value = isValidNumber('111222', { minChar: 10 });
+    value = isValidNumber('111222', { minChar: 10, maxChar: 10 });
     expect(value).toEqual(false);
 
-    value = isValidNumber('a', { minChar: 2 });
+    value = isValidNumber('a', { minChar: 2, maxChar: 10 });
     expect(value).toEqual(false);
   });
 
@@ -21,16 +21,16 @@ describe('Components:Form:modules:isValidNumber', () => {
     let value = isValidNumber('111222333', { minChar: 8 });
     expect(value).toEqual(true);
 
-    value = isValidNumber('11122233', { minChar: 6 });
+    value = isValidNumber('1122233', { minChar: 6, maxChar: 10 });
     expect(value).toEqual(true);
 
-    value = isValidNumber('111222333', { minChar: 2 });
+    value = isValidNumber('111222333', { minChar: 2, maxChar: 10 });
     expect(value).toEqual(true);
 
-    value = isValidNumber('111222333', { minChar: 3 });
+    value = isValidNumber('111222333', { minChar: 3, maxChar: 10 });
     expect(value).toEqual(true);
 
-    value = isValidNumber('0111222333', { minChar: 10 });
+    value = isValidNumber('0111222333', { minChar: 10, maxChar: 10 });
     expect(value).toEqual(true);
   });
 });
