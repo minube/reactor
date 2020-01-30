39f669905b726731441c9d904f6f3012a601e1cf
david <dgalindoc@gmail.com>
2020-01-30 14:07:21 +0100

    MIN-13804 - Refactor config

--- components/Form/modules/isValidNumber.js
+++ components/Form/modules/isValidNumber.js
@@ -1,6 +1,5 @@
 
 export default (number = '', { minChar = 0, maxChar = 999) => {
-  console.log(maxChar);
   const REGEXP = new RegExp(`^[0-9]{${minChar},${maxChar}}$`);
   return number !== '' ? REGEXP.test(number) : true;
 };
